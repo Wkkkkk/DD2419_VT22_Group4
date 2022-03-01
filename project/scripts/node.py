70ff154150a3bb4dd33a208dbdb1b9455ef6133a
ewarberg <99740591+ewarberg@users.noreply.github.com>
2022-03-01 09:17:09 -0500

    Add files via upload
    Started on a planner for ms3 with a complete planner class, and put Node and GridMap in other files

---
+++ project/scripts/node.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class Node:
+    """ Node class for the nodes in the grid map. """
+    def __init__(self, index, parent=None, position=np.zeros(3), yaw=0.0):
+        self.index = index
+        self.position = position
+        self.yaw = yaw
+        self.parent = parent
+        self.cost2go = 0
+        self.cost2come = 0
+        self.cost = 0
