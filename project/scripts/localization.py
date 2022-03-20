618a057010a3d72c8c003474320b8a8244241bc5
maciejw <wk707060335@gmail.com>
2022-03-20 14:33:19 +0100

    add kalman filter

--- project/scripts/localization.py
+++ project/scripts/localization.py
@@ -5,7 +5,7 @@ import rospy
 import tf2_ros
 import tf2_geometry_msgs
 import numpy as np
-from tf.transformations import quaternion_from_matrix, quaternion_matrix
+from tf.transformations import quaternion_from_matrix, quaternion_matrix, euler_from_quaternion, quaternion_from_euler
 from std_msgs.msg import Bool
 from geometry_msgs.msg import PoseStamped, TransformStamped
 from aruco_msgs.msg import MarkerArray
@@ -47,6 +47,104 @@ def transform_from_hcmatrix(hcm,header_frame,child_frame):
     return t
 
 
+def transform_to_pq(msg):
+    """Convert a C{geometry_msgs/Transform} into position/quaternion np arrays
+
+    @param msg: ROS message to be converted
+    @return:
+      - p: position as a np.array
+      - q: quaternion as a numpy array (order = [x,y,z,w])
+    """
+    p = np.array([msg.translation.x, msg.translation.y, msg.translation.z])
+    q = np.array([msg.rotation.x, msg.rotation.y,
+                  msg.rotation.z, msg.rotation.w])
+    return p, q
+
+
+def transform_stamped_to_pq(msg):
+    """Convert a C{geometry_msgs/TransformStamped} into position/quaternion np arrays
+
+    @param msg: ROS message to be converted
+    @return:
+      - p: position as a np.array
+      - q: quaternion as a numpy array (order = [x,y,z,w])
+    """
+    return transform_to_pq(msg.transform)
+
+
+class KalmanFilter:
+    def __init__(self):
+        self.is_initialized = False
+
+        # Motion model
+        self.A = np.eye(3)     # State transition matrix
+        self.R = np.eye(3)     # Motion noise matrix
+
+        # Measurement model
+        self.C = np.ones(3)    # Observation matrix. #array([[1,1,1]])
+        self.Q = np.eye(3)*0.1 # Observation noise matrix
+
+        self.mu = None
+        self.sigma = None
+        self.z = None
+
+
+    def update(self, msg):
+        p, q = transform_stamped_to_pq(msg)
+        roll, pitch, yaw = euler_from_quaternion(q)
+
+        # Measurement
+        self.z = np.array([p[0], p[1], yaw])  # x, y, yaw
+
+        if not self.is_initialized:
+            self.mu = self.z
+            self.sigma = np.eye(3)
+            self.is_initialized = True
+
+        # Estimated belief
+        self.mu, self.sigma = self.kf_predict()
+
+        # Posterior distribution and Kalman Gain
+        self.mu, self.sigma, K = self.kf_update()
+
+        # Output 
+        msg.transform.translation.x = self.mu[0]
+        msg.transform.translation.y = self.mu[1]
+
+        (msg.transform.rotation.x,
+         msg.transform.rotation.y,
+         msg.transform.rotation.z,
+         msg.transform.rotation.w) = quaternion_from_euler(0, 0, self.mu[2])  # yaw
+
+
+        return msg
+
+
+    def kf_update(self):
+        # Kalman Gain
+        K_term = np.dot(self.C.T, np.linalg.inv((np.dot(self.C, np.dot(self.sigma, self.C.T)) + self.Q)))
+        K = np.dot(self.sigma, K_term)
+        # Innovation
+        eta = self.z - np.dot(self.C, self.mu)
+
+        #print(self.z.shape, K.shape, eta.shape)
+        # Compute posterior
+        mu = self.mu + np.dot(K, eta) # Mean of the state
+        I = np.eye(3)
+        sigma = np.dot((I - np.dot(K, self.C)), self.sigma) # Covariance of the state
+
+        return mu, sigma, K
+
+    def kf_predict(self):
+        # Estimated mean of the state (no control)
+        mu = np.dot(self.A, self.mu)
+
+        # Estimated covariance of the state
+        sigma = np.dot(self.A, np.dot(self.sigma, self.A.T)) + self.R
+
+        return mu, sigma
+
+
 class Localization(object):
     def __init__(self):
         self.tf_buffer = tf2_ros.Buffer()
@@ -58,6 +156,9 @@ class Localization(object):
         self.transform.child_frame_id  = 'cf1/odom'
         self.transform.transform.rotation.w = 1
         self.is_initialized = False
+
+        self.kf = KalmanFilter()
+
 
     def marker_callback(self, msg):
         markers = msg.markers
@@ -86,7 +187,7 @@ class Localization(object):
             T = np.dot(M, O_inv)
             transform = transform_from_hcmatrix(T, 'map', 'cf1/odom')
             #print(transform)
-            self.transform = transform
+            self.transform = self.kf.update(transform)
 
 
 def main():
